4cc3f34447971ff1bca446b3e004d49aeeba9f94
0xPrakharG <prakhargoyal.3014@gmail.com>
2025-03-12 14:37:05 +0530

    product filter

---
+++ React/Day1/filter-list/src/components/FilterableProductTable.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import ProductTable from "./ProductTable";
+import SearchBar from "./SearchBar";
+
+const FilterableProductTable = ({ products }) => {
+    const [filterText, setFilterText] = useState("");
+    const [inStockOnly, setInStockOnly] = useState(false);
+
+    return (
+        <>
+            <SearchBar
+                filterText={filterText}
+                inStockOnly={inStockOnly}
+                onFilterTextChange={setFilterText}
+                onInStockOnlyChange={setInStockOnly}
+            />
+            <ProductTable
+                products={products}
+                filterText={filterText}
+                inStockOnly={inStockOnly}
+            />
+        </>
+    );
+};
+
+export default FilterableProductTable;
