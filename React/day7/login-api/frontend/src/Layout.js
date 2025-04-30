7cf2f045b7b29d3bbaf4d9526daf7cd68707c7e6
0xPrakharG <prakhargoyal.3014@gmail.com>
2025-04-30 16:33:52 +0530

    custom hook added

--- React/day7/login-api/frontend/src/Layout.js
+++ React/day7/login-api/frontend/src/Layout.js
@@ -1,12 +1,9 @@
 import { Link, Outlet, useNavigate } from "react-router-dom";
+import useAuth from "./hooks/useAuth";
 
 const Layout = () => {
     const navigate = useNavigate();
-
-    const handleLogout = () => {
-        localStorage.removeItem("token");
-        navigate("/login");
-    };
+    const { logout } = useAuth(navigate);
 
     return (
         <>
@@ -16,7 +13,7 @@ const Layout = () => {
                     <Link to="/about">About</Link>
                     <Link to="/user">User Data</Link>
                 </div>
-                <button onClick={handleLogout}>Logout</button>
+                <button onClick={logout}>Logout</button>
             </nav>
             <main className="main-content">
                 <Outlet />
--- React/day7/login-api/frontend/src/UserData.js
+++ React/day7/login-api/frontend/src/UserData.js
@@ -1,57 +1,17 @@
-import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import useAuth from "./hooks/useAuth";
 
 const UserData = () => {
     const navigate = useNavigate();
     let location = useLocation();
-
-    const [user, setUser] = useState(null);
-    const [error, setError] = useState("");
-
-    const fetchUserData = async () => {
-        const token = localStorage.getItem("token");
-
-        if (!token) {
-            navigate("/login");
-            return;
-        }
-
-        try {
-            const response = await fetch("http://localhost:3008/sign", {
-                method: "GET",
-                headers: {
-                    AUthorization: `Bearer ${token}`,
-                },
-            });
-
-            if (!response.ok) {
-                setError("Token expired or invalid");
-            }
-
-            const data = await response.json();
-            setUser(data.data[0].user);
-        } catch (error) {
-            setError("Error Occured", error);
-        }
-    };
-
-    const handleLogout = () => {
-        localStorage.removeItem("token");
-        navigate("/login");
-    };
-
-    useEffect(() => {
-        fetchUserData();
-    }, [navigate]);
+    const { user, error, loading } = useAuth(navigate);
 
     return (
         <div className="home-container">
-            <h1>
-                {location.pathname === "/"
+            <h1 style={{ textTransform: "capitalize" }}>
+                {location.pathname.split("/")[1] === ""
                     ? "Home"
-                    : location.pathname === "/about"
-                    ? "About"
-                    : "User"}{" "}
+                    : location.pathname.split("/")[1]}{" "}
                 Page
             </h1>
             {error && <p className="error">{error}</p>}
@@ -62,7 +22,7 @@ const UserData = () => {
                     </p>
                 </>
             ) : (
-                !error && <p>Loading user info...</p>
+                loading && !error && <p>Loading user info...</p>
             )}
         </div>
     );
